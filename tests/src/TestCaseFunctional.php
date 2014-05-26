aeaaf23d894862cb04c9c748c5ebf26425abe09f
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-05-26 19:17:32 +0200

    Setup functional tests

--- tests/src/TestCaseFunctional.php
+++ tests/src/TestCaseFunctional.php
@@ -6,12 +6,20 @@ class TestCaseFunctional extends TestCase {
 
     public function setUp() {
         \WP_Mock::setUp();
-        Brain::boot( new \Pimple, FALSE );
+        $brain = Brain::boot( new \Pimple, FALSE );
         global $wp;
         $wp = new \WP;
         if ( ! defined( 'CORTEXBASEPATH' ) ) {
             define( 'CORTEXBASEPATH', dirname( dirname( dirname( __FILE__ ) ) ) );
         }
+        $brain->addModule( new \Brain\Amygdala\BrainModule );
+        $brain->addModule( new \Brain\Striatum\BrainModule );
+        $brain->addModule( new \Brain\Cortex\BrainModule );
+    }
+
+    function tearDown() {
+        Brain::flush();
+        parent::tearDown();
     }
 
 }
