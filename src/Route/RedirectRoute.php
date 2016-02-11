9cf836fa72c433d95f5a65fcb9e7ba5a79fc3481
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-02-11 22:41:42 +0100

    Make RedirectRoute easier to instantiate

--- src/Route/RedirectRoute.php
+++ src/Route/RedirectRoute.php
@@ -27,26 +27,42 @@ final class RedirectRoute implements RouteInterface
      */
     private $route;
 
-    /**
-     * Route constructor.
-     *
-     * @param array                                        $data
-     * @param \Brain\Cortex\Controller\ControllerInterface $controller
-     */
-    public function __construct(array $data, ControllerInterface $controller = null)
+    public function __construct($from, $to, array $options, ControllerInterface $controller = null)
     {
+        $status = empty($options['redirect_status']) ? 301 : $options['redirect_status'];
+        in_array((int) $status, range(300, 308), true) or $status = 301;
+
+        $ext = empty($options['redirect_external']) ? false : $options['redirect_external'];
+        $ext = filter_var($ext, FILTER_VALIDATE_BOOLEAN);
+
+        $vars = [
+            'redirect_status'   => $status,
+            'redirect_external' => $ext,
+            'redirect_to'       => null,
+        ];
+
+        if (is_callable($to)) {
+            $vars = function (array $vars) use ($to, $status, $ext) {
+                $to = $to($vars);
+
+                return [
+                    'redirect_to'       => filter_var($to, FILTER_VALIDATE_URL) ? $to : null,
+                    'redirect_status'   => $status,
+                    'redirect_external' => $ext
+                ];
+            };
+        } elseif (filter_var($to, FILTER_VALIDATE_URL)) {
+            $vars['redirect_to'] = $to;
+        }
+
         $args = [
-            'path'    => empty($data['path']) ? null : $data['path'],
-            'vars'    => [
-                'redirect_to'       => empty($data['redirect_to']) ? null : $data['redirect_to'],
-                'redirect_status'   => empty($data['redirect_status']) ? 301 : $data['redirect_status'],
-                'redirect_external' => empty($data['redirect_external']) ? false : $data['redirect_external'],
-            ],
+            'path'    => $from,
+            'vars'    => $vars,
             'handler' => $controller ?: new RedirectController(),
         ];
 
-        isset($data['id']) and $args['id'] = $data['id'];
-        isset($data['merge_query_string']) and $args['merge_query_string'] = $data['merge_query_string'];
+        isset($options['id']) and $args['id'] = $options['id'];
+        isset($options['merge_query_string']) and $args['merge_query_string'] = $options['merge_query_string'];
 
         $this->route = new Route($args);
     }
