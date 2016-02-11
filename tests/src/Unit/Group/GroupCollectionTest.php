d07a95d06049cf5cd842996d39849acb4ab79604
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-02-12 00:49:35 +0100

    Add unit test for Group and GroupCollection

---
+++ tests/src/Unit/Group/GroupCollectionTest.php
@@ -0,0 +1,133 @@
+<?php
+/*
+ * This file is part of the cortex package.
+ *
+ * (c) Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brain\Cortex\Tests\Unit\Group;
+
+use Andrew\Proxy;
+use Brain\Cortex\Group\Group;
+use Brain\Cortex\Group\GroupCollection;
+use Brain\Cortex\Group\GroupInterface;
+use Brain\Cortex\Route\QueryRoute;
+use Brain\Cortex\Route\Route;
+use Brain\Cortex\Tests\TestCase;
+
+/**
+ * @author  Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @package cortex
+ */
+class GroupCollectionTest extends TestCase
+{
+
+    public function testAddGroup()
+    {
+        $group = \Mockery::mock(GroupInterface::class);
+        $group->shouldReceive('id')->andReturn('foo');
+
+        $collection = new GroupCollection();
+        $return = $collection->addGroup($group);
+
+        $proxy = new Proxy($collection);
+
+        /** @noinspection PhpUndefinedFieldInspection */
+        assertSame($proxy->groups, ['foo' => $group]);
+        assertSame($return, $collection);
+    }
+
+    public function testMergeGroup()
+    {
+        $route = new Route([
+            'id'    => 'my_route',
+            'group' => 'test_group',
+            'vars'  => [
+                'foo' => 'bar'
+            ]
+        ]);
+
+        $group = new Group([
+            'id'      => 'test_group',
+            'handler' => 'my_handler',
+            'host'    => 'example.com',
+            'vars'    => [
+                'meh' => 'meh'
+            ]
+        ]);
+
+        $expected = [
+            'id'      => 'my_route',
+            'group'   => 'test_group',
+            'vars'    => [
+                'foo' => 'bar'
+            ],
+            'handler' => 'my_handler',
+            'host'    => 'example.com'
+        ];
+
+        $collection = new GroupCollection();
+        $collection->addGroup($group);
+
+        /** @var \Brain\Cortex\Route\RouteInterface $newRoute */
+        $newRoute = $collection->mergeGroup($route);
+        $actual = $newRoute->toArray();
+
+        ksort($actual);
+        ksort($expected);
+
+        assertSame($expected, $actual);
+    }
+
+    public function testMergeMultipleGroups()
+    {
+        $route = new QueryRoute(
+            '/path/to',
+            ['name' => 'test'],
+            [
+                'paged' => true,
+                'group' => ['group_1', 'group_2', 'group_3']
+            ]
+        );
+
+        $group1 = new Group([
+            'id'      => 'group_1',
+            'handler' => 'my_handler',
+            'method'  => 'POST',
+            'vars'    => ['foo' => 'bar'],
+        ]);
+
+        $group2 = new Group([
+            'id'                 => 'group_2',
+            'paged'              => false,
+            'merge_query_string' => true
+        ]);
+
+        $expected = [
+            'id'                 => $route->id(),
+            'path'               => '/path/to',
+            'vars'               => ['name' => 'test'],
+            'paged'              => true,
+            'group'              => ['group_1', 'group_2', 'group_3'],
+            'handler'            => $route->offsetGet('handler'),
+            'method'             => 'POST',
+            'merge_query_string' => true,
+        ];
+
+        $collection = new GroupCollection();
+        $collection->addGroup($group1)->addGroup($group2);
+
+        /** @var \Brain\Cortex\Route\RouteInterface $newRoute */
+        $newRoute = $collection->mergeGroup($route);
+        $actual = $newRoute->toArray();
+
+        ksort($actual);
+        ksort($expected);
+
+        assertSame($expected, $actual);
+    }
+}
---
+++ tests/src/Unit/Group/GroupTest.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * This file is part of the cortex package.
+ *
+ * (c) Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brain\Cortex\Tests\Unit\Group;
+
+use Brain\Cortex\Group\Group;
+use Brain\Cortex\Tests\TestCase;
+
+/**
+ * @author  Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @package cortex
+ */
+class GroupTest extends TestCase
+{
+
+    public function testIdWhenGiven()
+    {
+        $group = new Group(['id' => 'test_me']);
+
+        assertSame('test_me', $group->id());
+    }
+
+    public function testIdWhenDefault()
+    {
+        $group = new Group([]);
+
+        assertStringMatchesFormat('group_'.spl_object_hash($group), $group->id());
+    }
+
+    public function testToArrayStripsInvalid()
+    {
+        $group = new Group([
+            'foo',
+            1,
+            'vars'     => [],
+            'group'    => 'nested',
+            'path'     => '/',
+            'handler'  => '__return_true',
+            'meh'      => 'meh',
+            'priority' => 0,
+            'id'       => 'test_me'
+        ]);
+
+        $expected = [
+            'vars'     => [],
+            'path'     => '/',
+            'handler'  => '__return_true',
+            'priority' => 0,
+            'id'       => 'test_me'
+        ];
+
+        assertSame($expected, $group->toArray());
+    }
+
+    public function testArrayAccess()
+    {
+        $group = new Group([
+            'foo',
+            1,
+            'vars'     => [],
+            'path'     => '/',
+            'handler'  => '__return_true',
+            'meh'      => 'meh',
+            'priority' => 0
+        ]);
+
+        assertFalse($group->offsetExists('foo'));
+        assertNull($group->offsetGet('meh'));
+        assertTrue($group->offsetExists('id')); // id is auto generated
+        assertTrue($group->offsetExists('vars'));
+        assertSame('/', $group->offsetGet('path'));
+        assertSame(0, $group->offsetGet('priority'));
+
+        unset($group['path']);
+        $group['priority'] = 1;
+        $group->offsetUnset('id');
+        $group->offsetUnset('vars');
+
+        assertNull($group->offsetGet('path'));
+        assertSame(1, $group->offsetGet('priority'));
+        assertTrue($group->offsetExists('id')); // id cannot be unset
+        assertFalse($group->offsetExists('vars'));
+        assertSame('__return_true', $group['handler']);
+    }
+
+}
