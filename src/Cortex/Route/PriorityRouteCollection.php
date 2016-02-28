fda35e132952854b71bf56ef8a1aedf0dcf3e6f0
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-02-28 17:59:21 +0100

    Improve auto paginated route route vars handler
    Auto-generated paginated routes now always receive 'paged' (or 'page')
    var and always return 'paged' (or 'page') query arg.

--- src/Cortex/Route/PriorityRouteCollection.php
+++ src/Cortex/Route/PriorityRouteCollection.php
@@ -133,10 +133,41 @@ final class PriorityRouteCollection implements RouteCollectionInterface
             $array['id'] = $route->id().'_paged';
             $array['paged'] = RouteInterface::PAGED_UNPAGED;
             $array['path'] = $pagedArg === RouteInterface::PAGED_ARCHIVE
-                ? $path.'/'.$base.'/{paged:\d+}'
-                : $path.'/{page:\d+}';
+                ? $path.'/'.$base.'/{paged:\d+}/?$'
+                : $path.'/{page:\d+}/?$';
+
+            $routeVars = $route->offsetExists('vars') ? $route->offsetGet('vars') : [];
+            if (is_callable($routeVars)) {
+                $array['vars'] = $this->buildPagedVars($routeVars, $pagedArg);
+            }
 
             return apply_filters('cortex.paged-route', new Route($array), $route);
         }
+    }
+
+    /**
+     * When route `vars` property is a callback and the route is paged, we ensure that the `vars`
+     * callable receives the proper `paged` (or `page`) query argument and also that it returned
+     * array includes `paged` (or `page`) query argument.
+     *
+     * @param  callable $routeVars
+     * @param  string   $pagedArg
+     * @return \Closure
+     */
+    private function buildPagedVars(callable $routeVars, $pagedArg)
+    {
+        $key = $pagedArg === RouteInterface::PAGED_SINGLE ? 'page' : 'paged';
+
+        return function (array $vars) use ($routeVars, $key) {
+            (isset($vars[$key]) && is_numeric($vars[$key])) or $vars[$key] = 1;
+            $vars[$key] = (int) $vars[$key];
+            $result = $routeVars($vars);
+            if (is_array($result) && ! (isset($result[$key]) && is_numeric($result[$key]))) {
+                $result[$key] = $vars[$key];
+            }
+            $result[$key] = (int) $result[$key];
+
+            return $result;
+        };
     }
 }
