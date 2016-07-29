8437d628bacf2972b5b81ced3bdbe8e1f01ea150
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-07-29 11:46:36 +0200

    Improve inline doc in for ResultHandler::setTemplate

--- src/Cortex/Router/ResultHandler.php
+++ src/Cortex/Router/ResultHandler.php
@@ -104,15 +104,6 @@ final class ResultHandler implements ResultHandlerInterface
             return;
         }
 
-        // If template is `false`, we return `true` on `"{$type}_template"`
-        // to speed up `template-loader.php`
-        $template_setter = $template !== false
-            ? function () use ($template) {
-                return $template;
-            }
-        : '__return_true';
-
-
         $types = [
             '404',
             'search',
@@ -132,14 +123,24 @@ final class ResultHandler implements ResultHandlerInterface
             'index',
         ];
 
+        $returnTemplate = function () use ($template) {
+            current_filter('template_include') and remove_all_filters('template_include');
+
+            return $template;
+        };
+
+        /*
+         * If template is `false`, we return `true` on `"{$type}_template"` to speed up
+         * `template-loader.php`, in fact, if template is `false` we are going to return `false` at
+         * 'template_include' anyway, so no need to waste time looping all template types, even
+         * because every type check for file(s) in filesystem, so it's a slow operation.
+         */
+        $template_setter = $template !== false ? $returnTemplate : '__return_true';
+
         array_walk($types, function ($type) use ($template_setter) {
             add_filter("{$type}_template", $template_setter);
         });
 
-        add_filter('template_include', function () use ($template) {
-            remove_all_filters('template_include');
-
-            return $template;
-        }, -1);
+        add_filter('template_include', $returnTemplate, -1);
     }
 }
